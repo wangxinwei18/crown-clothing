b0a701e7dafdd5d6860a2f9af376695cab333297
wangxinwei18 <wangxinwei18@hotmail.com>
2023-07-10 21:13:43 +0800

    finish lecture29 finish section158 build categories reducer to redux

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import App from "./App";
-import { CategoriesProvider } from "./contexts/categories.context";
 
 import { CartProvider } from "./contexts/cart.context";
 import { store } from "./store/store";
@@ -14,17 +13,15 @@ import "./index.scss";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <>
     <Provider store={store}>
       <BrowserRouter>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function

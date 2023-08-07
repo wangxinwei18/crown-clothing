0b0acf46d2e3ec0c65f9a7191b778e391a8d69a2
wangxinwei18 <wangxinwei18@hotmail.com>
2023-08-07 15:29:23 +0800

    on section21, learn TS but not completed

--- src/store/categories/category.selector.ts
+++ src/store/categories/category.selector.ts
@@ -1,6 +1,9 @@
 import { createSelector } from "reselect";
+
+import { CategoriesState } from "./category.reducer";
+import { CategoryMap } from "./category.types";
 // only return categories part of in all state
-const selectCategoryReducer = (state) => state.categories;
+const selectCategoryReducer = (state): CategoriesState => state.categories;
 export const selectCategories = createSelector(
   [selectCategoryReducer], // input
   (categoriesSlice) => categoriesSlice.categories // output
@@ -8,13 +11,13 @@ export const selectCategories = createSelector(
 
 export const selectCategoriesMap = createSelector(
   [selectCategories],
-  (categories) =>
+  (categories): CategoryMap =>
     //如果categories array 不变，不运行reduce,返回原先的值
     categories.reduce((acc, category) => {
       const { title, items } = category;
       acc[title.toLowerCase()] = items;
       return acc;
-    }, {})
+    }, {} as CategoryMap)
 );
 
 export const selectCategoriesIsLoading = createSelector(
